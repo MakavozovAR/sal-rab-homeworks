e9d6957cd90888e7214081ed4f98a133d849b4d5
miso test <aleksandr_makavozov@mail.ru>
2023-05-28 21:17:43 +0500

    Task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: `${name} ${phone}`,
+    order: {
+        address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`,
+        sum: sum
+    }, 
+    goods: []};
 
-    let countOfGoods = goods.length;
+// Проходимся по массиву goods, тк нам нужно изменить в нем данные 
+for (let i = 0; i < goods.length; i++) {
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+// Добавляем в свойство goods объекта data другой объект со свойствами title и count 
+data.goods.push({title: goods[i].title, count: goods[i].count});
+};
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+// Задаем в объекте data свойство data (так нужно по заданию)
+let jsonData = JSON.stringify({data: data});
+return jsonData;
 }
